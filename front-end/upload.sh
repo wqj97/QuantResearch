5d8ae111ce2c425e21ae03c8b25ac479d397a9ea
Wanqj <wqj97@126.com>
2018-09-24 20:59:56 +0800

    chore: ssl

--- front-end/upload.sh
+++ front-end/upload.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-IP=180.76.97.240
+IP=180.76.53.243
 USER=root
 source_dir=./build
-dest_dir=/home/wwwroot/www.lyquant.com/public/public
-key_path=./scripts/wanqj.key
+dist_dir=/root/QuantResearch/back-end/public/public
 #clean dir
 
 
 #upload file
-ssh $USER@$IP -i $key_path "rm -fr $dest_dir"
+ssh $USER@$IP "rm -fr $dist_dir"
 echo 正在上传文件
-scp -C -i $key_path -r $source_dir $USER@$IP:$dest_dir
+scp -C -r $source_dir $USER@$IP:$dist_dir
 echo 🎉🎉🎉
